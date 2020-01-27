903ae5f33b7014c4891e671bdd355b8c48c0275f
ambarkumarpandey <ambar.ara123@gmail.com>
2020-01-27 11:38:13 +0530

    BaseActivity Added

---
+++ app/src/main/java/com/example/applemusicanimation/ui/base/BaseActivity.kt
@@ -0,0 +1,33 @@
+package com.example.applemusicanimation.ui.base
+
+import android.os.Bundle
+import androidx.annotation.LayoutRes
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import javax.inject.Inject
+
+abstract class BaseActivity<B: ViewDataBinding,VM: ViewModel> : AppCompatActivity(){
+    lateinit var viewModel: VM
+    lateinit var binding: B
+
+    @Inject
+    lateinit var viewModelFactory: ViewModelProvider.Factory
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        bindView()
+    }
+    private fun bindView(){
+        binding = DataBindingUtil.setContentView(this,getLayoutId())
+        viewModel = ViewModelProvider(this,viewModelFactory).get(getViewModelClass())
+    }
+
+    @LayoutRes
+    abstract fun getLayoutId(): Int
+
+    abstract fun getViewModelClass(): Class<VM>
+
+}
