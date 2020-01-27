6490c022dacda9291187bfe9b6438b72c4517fae
ambarkumarpandey <ambar.ara123@gmail.com>
2020-01-27 13:10:06 +0530

    animation after click added

--- app/src/main/java/com/example/applemusicanimation/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/applemusicanimation/ui/main/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.example.applemusicanimation.ui.main
 
 import android.os.Bundle
+import android.view.View
 import com.example.applemusicanimation.R
 import com.example.applemusicanimation.databinding.ActivityMainBinding
 import com.example.applemusicanimation.ui.base.BaseActivity
 
-class MainActivity : BaseActivity<ActivityMainBinding, MainViewModel>() {
+class MainActivity : BaseActivity<ActivityMainBinding, MainViewModel>(), View.OnClickListener {
+    override fun onClick(v: View?) {
+
+    }
+
     override fun getLayoutId(): Int = R.layout.activity_main
 
     override fun getViewModelClass(): Class<MainViewModel> = MainViewModel::class.java
